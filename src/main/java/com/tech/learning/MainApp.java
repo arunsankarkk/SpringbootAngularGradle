8b9fdb2490ef4387585bd51150bea82435255aa3
Arun <arunsankarkk@gmail.com>
2018-05-08 00:01:04 -0400

    first working copy of Angular-SpringBoot

--- src/main/java/com/tech/learning/MainApp.java
+++ src/main/java/com/tech/learning/MainApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainApp.class, "welcome to spring with gradle");
+		SpringApplication.run(MainApp.class, args);
 		printOutput();
 	}
 	
